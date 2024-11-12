d02e5efa2acb72d4ddbe7ae6100d0ee1aa70e7a4
T0mm4s0Bry brignoli <brignoli.tommaso@iisgalvanimi.edu.it>
2024-11-12 10:20:34 +0000

    4 commit

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-@app.route('/piante/crea', methods=['POST'])
-def create_plant():
-    data = request.get_json()
-
-    scientific_name = data.get('scientific_name')
-    common_name = data.get('common_name')
-    type_ = data.get('type')
-    flower_color = data.get('flower_color')
-    height = data.get('height')
-
-    if not scientific_name or not common_name or not type_ or not flower_color or not height:
-        return jsonify({"errore": "Tutti i campi sono obbligatori"}), 400
-
+@app.route('/piante/elimina/<int:id>', methods=['DELETE'])
+def delete_plant(id):
     try:
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
 
-        query = "INSERT INTO Piante (scientific_name, common_name, type, flower_color, height) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(query, (scientific_name, common_name, type_, flower_color, height))
+        query = "DELETE FROM Piante WHERE id = %s"
+        cursor.execute(query, (id,))
         conn.commit()
 
-        return jsonify({"successo": "Nuova pianta aggiunta", "id": cursor.lastrowid}), 201
+        if cursor.rowcount == 0:
+            return jsonify({"errore": "Pianta non trovata"}), 404
+
+        return jsonify({"successo": "Pianta eliminata"}), 200
     
     except Error as e:
         print("Errore nella connessione al database:", e)
-        return jsonify({"errore": "Impossibile inserire la pianta nel database"}), 500
+        return jsonify({"errore": "Impossibile eliminare la pianta nel database"}), 500
     
     finally:
         if conn.is_connected():
             cursor.close()
             conn.close()
-
